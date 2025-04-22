9a342ded311485e87586be6ed5a89328e3136515
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-22 11:46:57 +0100

    Ajout de l’import et du montage des routes livres dans app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 const userRoutes = require("./routes/user.routes");
 const categoryRoutes = require("./routes/category.routes");
+const bookRoutes = require("./routes/book.routes");
 
 // Middleware pour parser les données JSON
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(express.json());
 // Routes
 app.use("/api/users", userRoutes); // Préfixe /api pour toutes les routes utilisateurs
 app.use("/api/categories", categoryRoutes); // Préfixe /api pour toutes les routes catégories
+app.use("/api/books", bookRoutes); // Préfixe /api pour toutes les routes catégories
 
 // Lancer le serveur
 const PORT = process.env.PORT || 3000;
