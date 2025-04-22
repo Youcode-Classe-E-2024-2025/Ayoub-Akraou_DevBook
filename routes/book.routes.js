0eb45302e928e996e3e2fede8a8fbce534f7d747
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-22 11:54:20 +0100

    Correction de l’import du contrôleur book dans book.routes.js

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bookController = require("../controllers/bookController");
+const bookController = require("../controllers/book.controller");
 
 // Routes RESTful
 router.get("/", bookController.index); // GET /books
