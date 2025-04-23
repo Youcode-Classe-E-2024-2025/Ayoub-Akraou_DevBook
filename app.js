143aa2c56f949522918e23c858a137eb3b3c2cf9
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-23 10:58:15 +0100

    Ajout de la route /api/emprunts pour la gestion des emprunts (import et montage du routeur emprunt)

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ const app = express();
 const userRoutes = require("./routes/user.routes");
 const categoryRoutes = require("./routes/category.routes");
 const bookRoutes = require("./routes/book.routes");
+const empruntRoutes = require("./routes/emprunt.routes");
 
 // Middleware pour parser les données JSON
 app.use(express.json());
+app.use(express.static("public"));
 
 // Routes
 app.use("/api/users", userRoutes); // Préfixe /api pour toutes les routes utilisateurs
 app.use("/api/categories", categoryRoutes); // Préfixe /api pour toutes les routes catégories
 app.use("/api/books", bookRoutes); // Préfixe /api pour toutes les routes catégories
+app.use("/api/emprunts", empruntRoutes); // Préfixe /api pour toutes les routes catégories
 
 // Lancer le serveur
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-app.use(express.static("public"));
